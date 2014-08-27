3c047b4f6802066dc7d1e01c8bf6e2adb748a00c
oscarotero <oom@oscarotero.com>
2014-08-27 21:51:05 +0200

    fixed #1

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-module.exports = {
-	Rule: function (rule) {
-		var prefix = rule.children('Selector').length > 1 ? ':matches(' + rule.selector + ')' : rule.selector;
-		var index = rule.index();
+module.exports = function (stylecow) {
+	return {
+		Rule: function (rule) {
+			var ruleSelectors = rule.children('Selector');
+			var index = rule.index();
 
-		rule.children('Rule').forEach(function (child, i) {
-			var selector = child.selector;
-			var space = ' ';
+			rule.children('Rule').forEach(function (child, i) {
+				child.children('Selector').forEach(function (childSelector) {
+					var space = ' ';
 
-			if (selector[0] === '&') {
-				space = '';
-				selector = selector.substr(1);
-			}
+					if (childSelector.name[0] === '&') {
+						childSelector.name = childSelector.name.substr(1);
+						space = '';
+					}
 
-			child.selector = prefix + space + (child.children('Selector').length > 1 ? ':matches(' + selector + ')' : selector);
-			rule.parent.add(child, index + i, true);
-		});
+					ruleSelectors.forEach(function (ruleSelector) {
+						var selector = stylecow.css.Selector.create(ruleSelector.name + space + childSelector.name);
+						child.add(selector);
+					});
+
+					childSelector.detach();
+				});
+
+				rule.parent.add(child, index + i, true);
+			});
+		}
 	}
 };
