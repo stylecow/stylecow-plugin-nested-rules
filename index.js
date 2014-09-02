0793dce550eac2d4c13d72319124bd2512d89432
oscarotero <oom@oscarotero.com>
2014-09-02 23:27:27 +0200

    Remove empty rules, support to preppend properties

--- index.js
+++ index.js
@@ -7,15 +7,32 @@ module.exports = function (stylecow) {
 			rule.children('Rule').forEach(function (child, i) {
 				child.children('Selector').forEach(function (childSelector) {
 					var space = ' ';
+					var prepend = false;
 
 					if (childSelector.name[0] === '&') {
 						childSelector.name = childSelector.name.substr(1);
 						space = '';
+
+						if (/^\w/.test(childSelector.name)) {
+							prepend = true;
+						}
 					}
 
 					ruleSelectors.forEach(function (ruleSelector) {
-						var selector = stylecow.css.Selector.create(ruleSelector.name + space + childSelector.name);
-						child.add(selector);
+						var code;
+
+						if (prepend) {
+							var content = ruleSelector.content;
+
+							code = content.slice(0, -1);
+							code.push(childSelector[0].name);
+							code.push(content.slice(-1));
+							code = code.concat(childSelector.slice(1)).join('');
+						} else {
+							code = ruleSelector.name + space + childSelector.name;
+						}
+
+						child.add(stylecow.css.Selector.create(code));
 					});
 
 					childSelector.detach();
@@ -23,6 +40,10 @@ module.exports = function (stylecow) {
 
 				rule.parent.add(child, index + i, true);
 			});
+
+			if (rule.children('Selector').length === rule.length) {
+				rule.remove();
+			}
 		}
 	}
 };
