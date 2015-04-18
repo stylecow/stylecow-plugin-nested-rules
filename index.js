be6fcfdc5babdd5ffdfb822c8b8419d33a049dcd
oscarotero <oom@oscarotero.com>
2015-04-18 18:04:01 +0200

    changes for stylecow-parser 2.0

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ module.exports = function (stylecow) {
 			var offset = 1;
 
 			parentRule
-				.firstChild({type: 'Block'})
-				.children()
+				.getChild('Block')
+				.getChildren()
 				.forEach(function (child) {
 
 					// resolve nested @media
@@ -26,7 +26,7 @@ module.exports = function (stylecow) {
 					}
 				});
 
-			if (!parentRule.firstChild('Block').length) {
+			if (!parentRule.getChild('Block').length) {
 				parentRule.remove();
 			}
 		}
@@ -35,39 +35,37 @@ module.exports = function (stylecow) {
 	function nestedMedia(parentRule, media, parentRuleIndex) {
 		var rule = new stylecow.Rule();
 
-		rule.push(parentRule.firstChild('Selectors').clone());
-		rule.push(media.firstChild('Block'));
+		rule.push(parentRule.getChild('Selectors').clone());
+		rule.push(media.getChild('Block'));
 
 		var block = new stylecow.Block();
 		block.push(rule);
-
 		media.push(block);
-
 
 		var index = rule.index();
 		var offset = 1;
 
 		rule
-			.firstChild({type: 'Block'})
-			.children('Rule')
+			.getChild('Block')
+			.getChildren('Rule')
 			.forEach(function (child) {
 				nestedRule(rule, child, index + offset);
 				++offset;
 			});
 
-		if (rule.firstChild('Block').length === 0) {
+		if (rule.getChild('Block').length === 0) {
 			rule.remove();
 		}
 
-		parentRule.parent().splice(parentRuleIndex, 0, media);
+		parentRule.getParent().splice(parentRuleIndex, 0, media);
 	}
 
 	function nestedRule(parentRule, rule, parentRuleIndex) {
-		var selectors = rule.firstChild({type: 'Selectors'});
+		var selectors = rule.getChild('Selectors');
 		var mergedSelectors = new stylecow.Selectors();
 
 		parentRule
-			.firstChild({type: 'Selectors'})
+			.getChild('Selectors')
 			.forEach(function (parentSelector) {
 				selectors.forEach(function (selector) {
 					mergedSelectors.push(merge(parentSelector.clone(), selector.clone()));
@@ -75,7 +73,7 @@ module.exports = function (stylecow) {
 			});
 
 		selectors.replaceWith(mergedSelectors);
-		parentRule.parent().splice(parentRuleIndex, 0, rule);
+		parentRule.getParent().splice(parentRuleIndex, 0, rule);
 	}
 
 	function merge (selector, appendedSelector) {
@@ -83,8 +81,7 @@ module.exports = function (stylecow) {
 
 		// html { .foo {  => html .foo
 		if (firstElement.type !== 'Combinator') {
-			var separator = new stylecow.Combinator();
-			separator.name = ' ';
+			var separator = (new stylecow.Combinator()).setName(' ');
 			selector.push(separator);
 			selector.push(firstElement);
 		}
@@ -95,13 +92,10 @@ module.exports = function (stylecow) {
 		}
 
 		// .foo { &html {  => html.foo
-		else if (appendedSelector.length && (appendedSelector[0].is({
-			type: 'Keyword',
-			name: /^\w/
-		}))) {
+		else if (appendedSelector.length && appendedSelector[0].is('TypeSelector')) {
 			firstElement = appendedSelector.shift();
 
-			var combinators = selector.children('Combinator');
+			var combinators = selector.getChildren('Combinator');
 
 			if (combinators.length) {
 				combinators.pop().after(firstElement);
